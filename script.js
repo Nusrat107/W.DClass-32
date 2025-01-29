2c9885fc10f65d9a2f502e14b88d31f826184a3b
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-01-29 11:50:29 +0600

    class-32

---
+++ script.js
@@ -0,0 +1,37 @@
+let x = 30;
+var y = 40;
+
+
+let amount;
+
+var number;
+
+const chocolate = 40;
+
+
+
+console.log('Hello everyone');
+
+amount =60;
+number = 4;
+
+
+amount = 80;
+number = 2;
+
+
+let totalMoney = 500;
+let egg = 5;
+let kitkat = 2;
+let eachEgg = 15;
+let eachKitkat = 40;
+
+let eggPrice = egg*eachEgg;
+let kitkatPrice = kitkat*eachKitkat;
+
+let totalPrice = eggPrice+kitkatPrice;
+
+let remainingMoney = totalMoney-totalPrice;
+
+console.log(remainingMoney);
+
